5e31917db119346e1a618983be0289ca0ca4c98f
Raoul Strackx <raoul.strackx@fortanix.com>
2021-11-15 14:02:54 +0100

    Hide not implemented error `set_read_timeout` and `set_write_timeout`

--- library/std/src/sys/unix/fortanixvme/net.rs
+++ library/std/src/sys/unix/fortanixvme/net.rs
@@ -313,12 +313,22 @@ impl TcpStream {
         self.inner
     }
 
-    pub fn set_read_timeout(&self, _dur: Option<Duration>) -> io::Result<()> {
-        not_available!()
+    pub fn set_read_timeout(&self, dur: Option<Duration>) -> io::Result<()> {
+        if dur == Some(Duration::default()) {
+            Err(io::Error::new(io::ErrorKind::InvalidInput, "cannot set a 0 duration timeout"))
+        } else {
+            // PLAT-368 provide proper implementation
+            Ok(())
+        }
     }
 
-    pub fn set_write_timeout(&self, _dur: Option<Duration>) -> io::Result<()> {
-        not_available!()
+    pub fn set_write_timeout(&self, dur: Option<Duration>) -> io::Result<()> {
+        if dur == Some(Duration::default()) {
+            Err(io::Error::new(io::ErrorKind::InvalidInput, "cannot set a 0 duration timeout"))
+        } else {
+            // PLAT-368 provide proper implementation
+            Ok(())
+        }
     }
 
     pub fn read_timeout(&self) -> io::Result<Option<Duration>> {
