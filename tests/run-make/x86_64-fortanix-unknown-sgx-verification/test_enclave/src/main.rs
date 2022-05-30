6b457811b4fc460f21f2f861b2e3d23ca3a493b8
Raoul Strackx <raoul.strackx@fortanix.com>
2022-05-30 14:27:03 +0200

    Verification of `image_base()`

--- tests/run-make/x86_64-fortanix-unknown-sgx-verification/test_enclave/src/main.rs
+++ tests/run-make/x86_64-fortanix-unknown-sgx-verification/test_enclave/src/main.rs
@@ -1,3 +1,14 @@
+#![feature(sgx_platform)]
+
+use std::os::fortanix_sgx::mem::image_base;
+
+#[no_mangle]
+#[inline(never)]
+pub fn get_image_base() -> u64 {
+    image_base()
+}
+
 fn main() {
     println!("Hello, world!");
+    println!("image base: {}", get_image_base());
 }
