55f542c38869d609818f407262540ef5c19aec34
Raoul Strackx <raoul.strackx@fortanix.com>
2022-06-20 12:16:02 +0200

    Verification of `read` usercall

--- tests/run-make/x86_64-fortanix-unknown-sgx-verification/verify.sh
+++ tests/run-make/x86_64-fortanix-unknown-sgx-verification/verify.sh
@@ -29,9 +29,10 @@ function build {
 
 build
 
-python3 verification/main.py ${enclave} "image_base"
-python3 verification/main.py ${enclave} "is_enclave_range"
-python3 verification/main.py ${enclave} "copy_to_userspace"
-python3 verification/main.py ${enclave} "insecure_time"
+#python3 verification/main.py ${enclave} "image_base"
+#python3 verification/main.py ${enclave} "is_enclave_range"
+#python3 verification/main.py ${enclave} "copy_to_userspace"
+#python3 verification/main.py ${enclave} "insecure_time"
+python3 verification/main.py ${enclave} "raw_read"
 
 echo "Verification completed successfully!"
